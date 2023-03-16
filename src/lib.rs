67db9cbb71bfb41a42cff1d59d60d3e09ce3aeb9
Matheus Xavier <mxavierdev@gmail.com>
2023-03-16 20:36:49 -0300

    fix missing pest

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 //! This crate provides a set of shared utilities for
 //! working with binary streams in memory or otherwise.
 
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
 pub mod const_fn;
 pub mod streams;
